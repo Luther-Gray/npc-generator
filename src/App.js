f78f81c45ef22aabf39c5b0131c77634f44fb2e3
Galen <galen.simmons0@gmail.com>
2024-02-21 19:10:54 -0500

    fixed link being too low

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ function App() {
         <h2>
           Character Generator
         </h2>
-        <p>Remember, these are just guidelines. Allow your creativity to twist the prompt past what you generated. You'll enjoy the process more that way. Promise❤️</p>
+        <p className='quote'>Remember, these are just guidelines. Allow your creativity to twist the prompt past what you generated.</p>
         <hr className="headerSeperator"></hr>
         <NPCGenerator/>
       </header>
-      <footer className="App-footer">
-        <p>Credits: Galen Simmons | Github - <a href="https://github.com/123789g" target="_blank" rel="noopener noreferrer">123789g</a></p>
+      <footer  className='appFooter'>
+        <p className='footerText'>Credits: Galen Simmons | Github - <a href="https://github.com/123789g" target="_blank" rel="noopener noreferrer">123789g</a></p>
       </footer>
     </div>
   );
